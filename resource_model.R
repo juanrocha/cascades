23fe7c7d7ab3e9c79a2e4039490d31ec2f8c1483
juanrocha <aguilajk@gmail.com>
2019-04-16 09:31:19 +0200

    update ccm

--- resource_model.R
+++ resource_model.R
@@ -6,6 +6,7 @@
 
 library(tidyverse)
 library(deSolve)
+library(deTestSet)
 library(rEDM)
 
 ## model
@@ -19,11 +20,11 @@ resource <- function(t, R, param){
         imports <- delta *(C_ij * A_ij) %*% R
 
         dR <- growth  - exports + imports + rnorm(n=n, mean = 0, sd = 1)
-        return(list(dR, exports, imports))
+        return(list(dR)) #,  exports, imports
     })
 }
 
-
+## J190415: Don't add exports and imports yet because in the model they are not modeled pair-wise, they are agregated. One would need to dissagregate or store the matrix instead of the vector. Where the vector now is the sum of the exports per species/country, while the matrix is the raw values.
 
 
 n <- 5 ## number of countries and resources
@@ -43,8 +44,13 @@ delta <-  1 # resource depletion coefficient (I dont' get why is necessary)
 times <- seq(from = 0, to = 100, by = 0.01)
 params <- list(alpha = alpha, c_allee = c_allee, K = K, C_ij = C_ij)
 
+
+## run the model
 print(system.time(
-    out <- ode(y = R, times = times, parms = params, func = resource)
+    out <- ode(
+      y = R, times = times, parms = params, func = resource,
+      method = "lsoda" ## see help("ode") for more methods
+    )
 ))
 
 
@@ -53,29 +59,90 @@ df <- out %>% as_data_frame() %>%
 
 
 ## plot result
+quartz(width = 4, height = 4)
 df %>%
     ggplot(aes(x=time, y=population)) +
-    geom_line(aes(color = species), show.legend = T) +
+    geom_line(aes(color = species), size = 0.25, show.legend = T) +
     theme_light()
 
 ## Test of CCM as identification method:
 ## follow my own code from 04-DataExploration.R
-
+lib <- c(1,2500)
+pred <- c(2501, 10001)
 ## Embedding dimension:
 # emb <- list()
 # for (i in 2:dim(out)[2]){
 #     emb[[i-1]] <- out[c(1,i)] %>%
 #         simplex()
 # }
+simplex_output <- out %>%
+    as.data.frame() %>%
+    select(-time) %>%
+    map(simplex, lib, pred)
 
-emb <- simplex(out[,c(1,2)])
-plot(emb$E, emb$rho, type = "l", xlab = "Embedding Dimension (E)", ylab = "Forecast Skill (rho)")
+bestE <- simplex_output %>% map_dbl(~ .$E[which.max(.$rho)] )
 
+sps <- c(1:5)
 
+for (i in seq_along(sps)){simplex_output[[i]]$species <- sps[i]}
 
+simplex_output <- simplex_output %>%
+  bind_rows()
+
+simplex_output %>%
+  ggplot(aes(E, rho)) +
+  geom_line(aes(group = species), size = 0.3) +
+  labs(x="Embeding dimension (E)", y = "Forecast skill (rho)") +
+  theme_light(base_size = 9)
+
+## Prediction decay
+prediction_decay <- out %>%
+  as.data.frame() %>%
+  select(-time) %>%
+  map(simplex, lib, pred, E = 2, tp = 1:10)
+
+for (i in seq_along(sps)){prediction_decay[[i]]$species <- sps[i]}
+
+prediction_decay %>%
+  bind_rows() %>%
+  ggplot(aes(tp, rho)) +
+  geom_line(aes(group = species), size = 0.3) +
+  labs(x="Time to prediction", y = "Forecast skill (rho)") +
+  theme_light(base_size = 9)
+
+## Non-linearity
+non_linear <- out %>%
+  as.data.frame() %>%
+  select(-time) %>%
+  as.list() %>%
+  map(., .f = s_map, E = 2, lib = lib, pred = pred) # this takes ages.
+
+## does not work with map2 - for now fixing E = 2
+## J190416: Note that E is a list or vector on which the function also parellize, so it has to be before the .function declaration.
+
+# print(system.time((s_map(out[[1]], lib, pred, E = 2))))
+for (i in seq_along(sps)){non_linear[[i]]$species <- sps[i]}
+
+non_linear %>%
+  bind_rows() %>%
+  ggplot(aes(theta, rho)) +
+  geom_line(aes(group = species), size = 0.3) +
+  labs(x="Nonlinearity (theta)", y = "Forecast skill (rho)") +
+  theme_light(base_size = 9)
+
+########
 
 ind <- crossing(
     lib_column = colnames(out)[-1],
     target_column = colnames(out)[-1])
 
 ind <- ind %>% filter(lib_column != target_column)
+
+rho_list <- map2(
+    .x = ind$lib_column, .y = ind$target_column ,
+    .f = ~ ccm(block = out, E = 2,
+        lib_column = .x, target_column = .y,
+        lib_sizes = seq(10,dim(out)[1], by = 100),
+        replace = FALSE, silent = TRUE,
+        random_libs = FALSE)
+    )
